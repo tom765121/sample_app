425cfdb9ff62ab19825d8b1ba44419fd93ed643b
tom chang <tom765121@yahoo.com.tw>
2017-12-15 05:44:01 +0000

    add static page

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get Home" do
+    get static_pages_Home_url
+    assert_response :success
+  end
+
+  test "should get Help" do
+    get static_pages_Help_url
+    assert_response :success
+  end
+
+end
