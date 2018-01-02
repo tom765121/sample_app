be09471b834fc5e9686103d0466c931aa01a5bb2
tom chang <tom765121@yahoo.com.tw>
2018-01-02 10:51:11 +0000

    add new testing

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -18,16 +18,16 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get about" do
-	get about_path
-	# static_pages_about_url
-	assert_response :success
-	assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+		get about_path
+		# static_pages_about_url
+		assert_response :success
+		assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get contact" do
-	get contact_path
-	# static_pages_contact_url
-	assert_response :success
-	assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+		get contact_path
+		# static_pages_contact_url
+		assert_response :success
+		assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 end
