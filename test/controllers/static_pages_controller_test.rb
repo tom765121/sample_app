7eba4e8aa080a1cc1164e3640734b387b5942bca
tom chang <tom765121@yahoo.com.tw>
2018-01-02 08:47:26 +0000

    add contact page and add css, routes mapping

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,21 +3,31 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get home" do
-    get static_pages_home_url
+    get home_path
+    # static_pages_home_url
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
+    # get helf_path
+    # static_pages_help_url
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
-  	get static_pages_about_url
-  	assert_response :success
-  	assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+	get about_path
+	# static_pages_about_url
+	assert_response :success
+	assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get contact" do
+	get contact_path
+	# static_pages_contact_url
+	assert_response :success
+	assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end
 end
