80d0cf090c14fcdc284559bb4b78d66df5d903ad
tom chang <tom765121@yahoo.com.tw>
2018-01-02 11:13:01 +0000

    add user controller and test case

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   # get 'static_pages/home'
   get '/home', to: 'static_pages#home'
 
@@ -11,6 +13,8 @@ Rails.application.routes.draw do
 
   # get 'static_pages/contact'
   get '/contact', to: 'static_pages#contact'
+
+  get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#home"
 
