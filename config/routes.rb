df9fb84eb21cbeff882fd52c353ff0d47e4715d3
tom chang <tom765121@yahoo.com.tw>
2018-02-09 08:21:53 +0000

    add sign up to code

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/signup', to: "users#new"
+  resources :users
 
   get "/index", to: 'static_pages#index'
 
@@ -8,6 +8,13 @@ Rails.application.routes.draw do
   get "/help", to: 'static_pages#help'
 
   get "/about", to: "static_pages#about"
+
+  get '/signup', to: "users#new"
+
+  post '/signup', to: "users#create"
+
+  get '/login', to: "users#login"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#home"
 end
