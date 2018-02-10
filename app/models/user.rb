421eaedf5f1df5747e2adeb8834acfebb9745f4f
tom chang <tom765121@yahoo.com.tw>
2018-02-10 07:49:08 +0000

    add_before_save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	before_save {self.email = self.email.downcase}
 	validates :name, presence: true, length: {maximum: 50, minimum: 5}
 	VALID_EMAIL_REGEX = /\A[\w+.\-]+@([a-z\d\-]+\.)+[a-z]+\z/i
 	validates :email, presence: true, length: {maximum: 250, minimum: 5}, format: { with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
